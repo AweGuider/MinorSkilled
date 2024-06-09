2d5a527dad9a98c9bfa2c9439d6256a47984d87b
AweGuider <aweguider@gmail.com>
2024-06-09 14:36:53 +0200

    [Feature : High Score] Adds High Score visual UI and C++ system, connects C++ to BP to UI, changes accessibility parameters of variables, creates general UI manager, Pause Menu's Resume button now works as well

--- Source/MinorSkilled/Public/Snowboarder.h
+++ Source/MinorSkilled/Public/Snowboarder.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this character's properties
 	ASnowboarder();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	UFUNCTION(BlueprintCallable, Category = "Snowboard")
 		void CalculateVelocity(FVector CurrentVelocity);
 
@@ -36,112 +39,64 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		bool FreeCameraEnabled;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		bool DisableSnowboard;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		FVector SnowboardForwardVector;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		float TurningSpeed;
 
 #pragma region Debug
 	
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FVector DebugArrowsOffset;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SidewaysInputColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugSlopeNormal;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SlopeNormalColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugGravityForce;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor GravityForceColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugSlopeProjection;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SlopeProjectionColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugSnowboardForward;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SnowboardForwardColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugSnowboardForwardProjection;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SnowboardForwardProjectionColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugVelocity;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor VelocityColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugSideways;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor SidewaysVectorColor;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		bool DebugFriction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Debug")
 		FLinearColor FrictionForceColor;
 
 #pragma endregion
-
-	// Change to BlueprintReadOnly in later stages, keep for now for showcase
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		FVector OutHitNormal;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		bool Grounded;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		float SnowboardRotationInterpSpeed;
-
-
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		float GravityForceMultiplier;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		float FrictionCoefficient;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		float MoveSpeed;
-
-#pragma region Unused
-
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		bool PhysicsEnabled;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom")
-		bool CanMove;
-
-#pragma endregion
-
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
@@ -150,21 +105,62 @@ protected:
 	UPROPERTY(BlueprintReadOnly, Category = "Custom")
 		float GravityConstant;
 
-	// Change to BlueprintReadOnly in later stages, keep for now for showcase
 	UPROPERTY(BlueprintReadWrite, Transient, Category = "Physics")
 		FVector GravityForce;
 
-	// Change to BlueprintReadOnly in later stages, keep for now for showcase
 	UPROPERTY(BlueprintReadWrite, Transient, Category = "Physics")
 		FVector FrictionForce;
 
-	// Change to BlueprintReadOnly in later stages, keep for now for showcase
 	UPROPERTY(BlueprintReadWrite, Transient, Category = "Physics")
 		FVector NewVelocity;
 
+	UPROPERTY(BlueprintReadWrite, Transient, Category = "Physics")
+		FVector SnowboardForwardVector;
+
+	UPROPERTY(BlueprintReadWrite, Transient, Category = "Physics")
+		FVector OutHitNormal;
+
 private:
+
+	// BlueprintReadOnly
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		bool FreeCameraEnabled;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		float TurningSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		bool Grounded;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		float SnowboardRotationInterpSpeed;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		float GravityForceMultiplier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		float FrictionCoefficient;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
+		float MoveSpeed;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom", meta = (AllowPrivateAccess = "true"))
 		float LineTraceLength;
+
+	// BlueprintReadWrite
+
+#pragma region Unused
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Unused", meta = (AllowPrivateAccess = "true"))
+		bool PhysicsEnabled;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Unused", meta = (AllowPrivateAccess = "true"))
+		bool CanMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Unused", meta = (AllowPrivateAccess = "true"))
+		bool DisableSnowboard;
+#pragma endregion
 };
 
